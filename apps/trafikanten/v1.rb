76e4ad2c6690e01e9980a2ae6abcd4b6fcbe9bc2
Rune Botten <rbotten@gmail.com>
2010-05-25 18:07:07 +0200

    Write tests for the Trafikanten V1 route API and make them pass

--- apps/trafikanten/v1.rb
+++ apps/trafikanten/v1.rb
@@ -11,7 +11,23 @@ class GuerillaAPI::Apps::Trafikanten::V1 < Sinatra::Base
   #       repeatedly by appending bogus GET params, or different JSONP callbacks.
   get '/route/:from_id/:to_id/:date/:time' do
     cache_forever
-    {:this => 'That'}.to_json
+    
+    begin
+      route = find_route(params[:from_id], params[:to_id])
+    rescue TrafikantenTravel::Error => e
+      status 400
+      return e.message
+    end
+    
+    time = Time.parse "#{params[:date]} #{params[:time]}"
+    route = find_route(params[:from_id], params[:to_id], time)
+    
+    if route.steps == []
+      status 404
+      return nil
+    end
+    
+    route_to_json(route)
   end
 
   # Next departure
@@ -20,8 +36,26 @@ class GuerillaAPI::Apps::Trafikanten::V1 < Sinatra::Base
   #       Varnish works on exact URL, so someone could hit our backend
   #       repeatedly by appending bogus GET params, or different JSONP callbacks.
   get '/route/:from_id/:to_id' do
-    #route = TrafikantenTravel::Route.new(params[:from_id], params[:to_id], Time.now)
-    {:this => 'That'}.to_json
+    # Error happened
+    begin
+      route = find_route(params[:from_id], params[:to_id])
+    rescue TrafikantenTravel::Error => e
+      status 400
+      return e.message
+    end
+    
+    # No route found
+    if route.steps == []
+      cache_forever
+      status 404
+      return nil
+    end
+    
+    time_until_departure = route.steps[0].depart[:time]
+    # Cache intil the next departure. Include the minute for the departure
+    expires ((time_until_departure - Time.now).to_i + 60), :public
+    
+    route_to_json(route)
   end
 
   # Search for stations
@@ -31,7 +65,6 @@ class GuerillaAPI::Apps::Trafikanten::V1 < Sinatra::Base
   #       repeatedly by appending bogus GET params, or different JSONP callbacks.
   get '/stations/:name' do
     cache_forever
-
     stations = TrafikantenTravel::Station.find_by_name params[:name]
     {:stations => stations.map do |station| 
       has_geo = station.lat && station.lng
@@ -45,7 +78,31 @@ class GuerillaAPI::Apps::Trafikanten::V1 < Sinatra::Base
 
   private
   
+  def route_to_json(route)
+    {
+      'route' => {
+        'duration' => route.duration,
+        'steps' => route.steps.map do |step|
+          {
+            'type' => step.type,
+            'line' => step.line,
+            'duration' => step.duration,
+            'depart' => step.depart,
+            'arrive' => step.arrive
+          }
+        end
+      }
+    }.to_json
+  end
+  
   def cache_forever
     expires 30000000, :public
   end
+  
+  def find_route(from, to, time = Time.now)
+    from  = TrafikantenTravel::Station.new({:id => from})
+    to    = TrafikantenTravel::Station.new({:id => to})
+    TrafikantenTravel::Route.find(from, to, time)
+  end
+  
 end
--- spec/apps/trafikanten_spec.rb
+++ spec/apps/trafikanten_spec.rb
@@ -12,6 +12,8 @@ describe GuerillaAPI::Apps::Trafikanten::V1 do
       :lng => '456',
       :type => 1
     })
+    
+    @mock_route = TrafikantenTravel::Route.new(@mock_station, @mock_station)
   end
   
   context 'searching for stations' do
@@ -24,6 +26,12 @@ describe GuerillaAPI::Apps::Trafikanten::V1 do
       it 'delivers json in utf-8' do
         get '/api/trafikanten/v1/stations/Stensberg'
         last_response.headers['Content-Type'].should == "application/json;charset=utf-8"
+      end
+      
+      it 'delivers JSONP when requested' do
+        get '/api/trafikanten/v1/stations/Stensberg?callback=func'
+        last_response.headers['Content-Type'].should == "application/javascript;charset=utf-8"
+        last_response.body.should =~ /^func\(/
       end
 
       it 'looks up stations by name and returns array of stations' do
@@ -79,35 +87,217 @@ describe GuerillaAPI::Apps::Trafikanten::V1 do
       context '(/route/:from/:to)' do
         
         it 'delivers json in utf-8' do
+          TrafikantenTravel::Route.stub(:find).and_return @mock_route
           get '/api/trafikanten/v1/route/1234/1234'
           last_response.headers['Content-Type'].should == "application/json;charset=utf-8"
         end
-
-        it 'returns 400 when an error occurred at trafikanten.no'
-
-        it 'returns 404 when route was not found' do
-          TrafikantenTravel::Route.stub(:parse).and_return {}
-
-          get '/api/trafikanten/v1/route/1234/1234'
-          last_response.status.should == 404
-          last_response.body.should == 'Ingen forbindelse funnet eller ingen stoppesteder funnet'
+        
+        it 'delivers JSONP when requested' do
+          TrafikantenTravel::Route.stub(:find).and_return @mock_route
+          get '/api/trafikanten/v1/route/1234/1234?callback=func'
+          last_response.headers['Content-Type'].should == "application/javascript;charset=utf-8"
+          last_response.body.should =~ /^func\(/
         end
 
-        it 'caches until the next departure'
-        #last_response.headers['Cache-Control'].should == "public, max-age="
+        it 'returns 400 when an normal error occurred at trafikanten.no' do
+          TrafikantenTravel::Route.stub(:find).and_raise TrafikantenTravel::Error.new('Bad things happened')
+          get '/api/trafikanten/v1/route/1234/1234'
+          last_response.status.should == 400
+          last_response.body.should == 'Bad things happened'          
+        end
+        
+        it 'returns 400 when an abnormal error occurred at trafikanten.no' do
+          TrafikantenTravel::Route.stub(:find).and_raise TrafikantenTravel::BadRequest.new('Very bad things happened')
+          get '/api/trafikanten/v1/route/1234/1234'
+          last_response.status.should == 400
+          last_response.body.should == 'Very bad things happened'
+        end
+        
+        context 'no route found' do
+          before(:each) do
+            TrafikantenTravel::Route.stub(:find).and_return @mock_route
+            get '/api/trafikanten/v1/route/1234/1234'            
+          end
+          
+          it 'returns 404' do
+            last_response.status.should == 404
+          end
+
+          it 'caches forever' do
+            last_response.headers['Cache-Control'].should == "public, max-age=30000000"
+          end
+          
+          it 'returns no body' do
+            last_response.body.should == ''
+          end
+          
+        end
+
+        it 'caches until the next departure' do
+          mocked = @mock_route.dup
+          time_now = Time.now
+          Time.stub(:now).and_return time_now
+          
+          first_step = TrafikantenTravel::Route::Step.new
+          first_step.depart = {
+            :station => 'Tullestasjonen',
+            :time => time_now + 120 # In 2 minutes
+          }
+          mocked.steps << first_step
+          TrafikantenTravel::Route.stub(:find).and_return mocked
+          
+          get '/api/trafikanten/v1/route/1234/1234'
+          last_response.headers['Cache-Control'].should == "public, max-age=180"
+        end
+        
+        it 'returns the correct structure' do
+          time_now = Time.now
+          Time.stub(:now).and_return time_now
+          
+          fully_mocked = TrafikantenTravel::Route.new(TrafikantenTravel::Station.new, TrafikantenTravel::Station.new, time_now)
+          
+          duration = 0
+          4.times do |s|
+            step = TrafikantenTravel::Route::Step.new
+            step.duration = rand(20) +1 
+            
+            step.depart = {
+              :station => "Startstasjon #{s}",
+              :time => time_now + duration * 60
+            }
+            
+            step.arrive = {
+              :station => "Stopstasjon #{s}",
+              :time => time_now + step.duration * 60
+            }
+            
+            step.type = :train
+            
+            duration = duration + step.duration
+            fully_mocked.steps << step
+          end
+          
+          fully_mocked.duration = duration
+
+          TrafikantenTravel::Route.stub(:find).and_return fully_mocked
+          get '/api/trafikanten/v1/route/12345/12345'
+          result = JSON.parse(last_response.body)
+          
+          # Full duration
+          route = result['route']
+          route['duration'].should == duration
+          
+          # First step
+          step = route['steps'][0]
+          step['depart']['station'].should == 'Startstasjon 0'
+          step['arrive']['station'].should == 'Stopstasjon 0'
+          step['type'].should == 'train'
+
+          # Last step
+          step = route['steps'][3]
+          step['depart']['station'].should == 'Startstasjon 3'
+          step['arrive']['station'].should == 'Stopstasjon 3'
+          step['type'].should == 'train'
+        end
+        
       end
     end
+    
     context 'at a specific time and date' do
 
       context '(/route/:from/:to/:date/:time)' do
+
+        it 'delivers json in utf-8' do
+          TrafikantenTravel::Route.stub(:find).and_return @mock_route
+          get '/api/trafikanten/v1/route/1234/1234/2010-04-29/12:00'
+          last_response.headers['Content-Type'].should == "application/json;charset=utf-8"
+        end
+
+        it 'delivers JSONP when requested' do
+          TrafikantenTravel::Route.stub(:find).and_return @mock_route
+          get '/api/trafikanten/v1/route/1234/1234/2010-04-29/12:00?callback=func'
+          last_response.headers['Content-Type'].should == "application/javascript;charset=utf-8"
+          last_response.body.should =~ /^func\(/
+        end
+        
         it 'caches forever' do
+          TrafikantenTravel::Route.stub(:find).and_return @mock_route
           get '/api/trafikanten/v1/route/1234/1234/2010-04-29/12:00'
           last_response.headers['Cache-Control'].should == "public, max-age=30000000"
         end
 
-        it 'returns 404 when no route was found'
+        it 'returns 404 when no route was found' do
+          TrafikantenTravel::Route.stub(:find).and_return @mock_route
+          get '/api/trafikanten/v1/route/1234/1234/2010-04-29/12:00'
+          last_response.status.should == 404
+          last_response.body.should == ''
+        end
+        
+        it 'returns 400 when an normal error occurred at trafikanten.no' do
+          TrafikantenTravel::Route.stub(:find).and_raise TrafikantenTravel::Error.new('Bad things happened')
+          get '/api/trafikanten/v1/route/1234/1234/2010-04-29/12:00'
+          last_response.status.should == 400
+          last_response.body.should == 'Bad things happened'          
+        end
+        
+        it 'returns 400 when an abnormal error occurred at trafikanten.no' do
+          TrafikantenTravel::Route.stub(:find).and_raise TrafikantenTravel::BadRequest.new('Very bad things happened')
+          get '/api/trafikanten/v1/route/1234/1234/2010-04-29/12:00'
+          last_response.status.should == 400
+          last_response.body.should == 'Very bad things happened'
+        end
+        
+        it 'returns the correct structure' do
+          time_now = Time.now
+          Time.stub(:now).and_return time_now
+          
+          fully_mocked = TrafikantenTravel::Route.new(TrafikantenTravel::Station.new, TrafikantenTravel::Station.new, time_now)
+          
+          duration = 0
+          4.times do |s|
+            step = TrafikantenTravel::Route::Step.new
+            step.duration = rand(20) +1 
+            
+            step.depart = {
+              :station => "Startstasjon #{s}",
+              :time => time_now + duration * 60
+            }
+            
+            step.arrive = {
+              :station => "Stopstasjon #{s}",
+              :time => time_now + step.duration * 60
+            }
+            
+            step.type = :train
+            
+            duration = duration + step.duration
+            fully_mocked.steps << step
+          end
+          
+          fully_mocked.duration = duration
 
-      end      
+          TrafikantenTravel::Route.stub(:find).and_return fully_mocked
+          get '/api/trafikanten/v1/route/12345/12345/2010-04-29/12:00'
+          result = JSON.parse(last_response.body)
+          
+          # Full duration
+          route = result['route']
+          route['duration'].should == duration
+          
+          # First step
+          step = route['steps'][0]
+          step['depart']['station'].should == 'Startstasjon 0'
+          step['arrive']['station'].should == 'Stopstasjon 0'
+          step['type'].should == 'train'
+
+          # Last step
+          step = route['steps'][3]
+          step['depart']['station'].should == 'Startstasjon 3'
+          step['arrive']['station'].should == 'Stopstasjon 3'
+          step['type'].should == 'train'
+        end
+        
+      end
     end
   end
   
