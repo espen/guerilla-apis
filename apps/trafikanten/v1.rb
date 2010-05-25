e95fef2c1833d7cbbf49333e6da2d0b25953abb6
Rune Botten <rbotten@gmail.com>
2010-05-25 16:37:47 +0200

    Add TODOs to Trafikanten V1

--- apps/trafikanten/v1.rb
+++ apps/trafikanten/v1.rb
@@ -5,35 +5,47 @@ class GuerillaAPI::Apps::Trafikanten::V1 < Sinatra::Base
   end
   
   # Specific departure
+  #
+  # TODO: Cache also in Memcache if more params than required is sent
+  #       Varnish works on exact URL, so someone could hit our backend
+  #       repeatedly by appending bogus GET params, or different JSONP callbacks.
   get '/route/:from_id/:to_id/:date/:time' do
     cache_forever
     {:this => 'That'}.to_json
   end
 
   # Next departure
+  #
+  # TODO: Cache also in Memcache if more params than required is sent
+  #       Varnish works on exact URL, so someone could hit our backend
+  #       repeatedly by appending bogus GET params, or different JSONP callbacks.
   get '/route/:from_id/:to_id' do
     #route = TrafikantenTravel::Route.new(params[:from_id], params[:to_id], Time.now)
     {:this => 'That'}.to_json
   end
 
   # Search for stations
+  #
+  # TODO: Cache also in Memcache if more params than :name is sent
+  #       Varnish works on exact URL, so someone could hit our backend
+  #       repeatedly by appending bogus GET params, or different JSONP callbacks.
   get '/stations/:name' do
     cache_forever
+
     stations = TrafikantenTravel::Station.find_by_name params[:name]
-    {:stations => stations.map{|s| station_to_json(s) }}.to_json
+    {:stations => stations.map do |station| 
+      has_geo = station.lat && station.lng
+      { :name => station.name,
+        :id => station.id,
+        :geo => has_geo ? {'Type'=>'Point','coordinates'=>[station.lng,station.lat]} : nil
+      }
+      end
+    }.to_json
   end
 
   private
   
   def cache_forever
     expires 30000000, :public
-  end
-  
-  def station_to_json(station)
-    has_coordinates = station.lat && station.lng
-    { :name => station.name,
-      :id => station.id,
-      :geo => has_coordinates ? {'Type'=>'Point','coordinates'=>[station.lng,station.lat]} : nil
-    }
   end
 end
