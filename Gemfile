1933df177a8db1fdb724e291e2ebbf7204339aa3
Espen Antonsen <espen@inspired.no>
2010-09-24 12:15:04 +0200

    find, not by id. return info for all stations. using bysykel 0.3.0

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ gem 'rack', '1.1.0'
 gem 'json'
 gem 'sinatra', '1.0'
 gem 'trafikanten-travel', '0.2.4', :require => 'trafikanten_travel'
-gem 'bysykkel', '0.2.0', :require => 'bysykkel'
+gem 'bysykkel', '0.3.0', :require => 'bysykkel'
 
 # We only need jsonp from rack-contrib
 gem 'rack-contrib', '1.0.1', :require => 'rack/contrib/jsonp'
--- apps/bysykkel/v1.rb
+++ apps/bysykkel/v1.rb
@@ -9,12 +9,12 @@ class GuerillaAPI::Apps::Bysykkel::V1 < Sinatra::Base
   # TODO: Cache also in Memcache if more params than :name is sent
   #       Varnish works on exact URL, so someone could hit our backend
   #       repeatedly by appending bogus GET params, or different JSONP callbacks.
-  get '/stations/:id' do
+  get '/station/:id' do
     cache_forever
     find_station params[:id]
   end
 
-  get '/stations/' do
+  get '/station/' do
     cache_forever
     all_stations
   end
@@ -29,15 +29,31 @@ class GuerillaAPI::Apps::Bysykkel::V1 < Sinatra::Base
     stations = Bysykkel::Station.all()
     {
       :source => 'smartbikeportal.clearchannel.no',
-      :stations => stations
+      :stations => stations.map do |station| 
+      {
+        'id' => station.id,
+        'ready_bikes' => station.ready_bikes,
+        'empty_locks' => station.empty_locks,
+        'online' => station.online,
+        'description' => station.description,
+        'geo' => has_geo ? {'Type'=>'Point','coordinates'=>[station.lng,station.lat]} : nil
+      }
+      end
     }.to_json
   end
 
   def find_station(id)
-    station = Bysykkel::Station.find_by_id(id)
+    station = Bysykkel::Station.find(id)
     {
       :source => 'smartbikeportal.clearchannel.no',
-      :stations => station
+      has_geo = station.lat && station.lng
+      :stations => {
+        'id' => station.id,
+        'ready_bikes' => station.ready_bikes,
+        'empty_locks' => station.empty_locks,
+        'online' => station.online,
+        'description' => station.description,
+        'geo' => has_geo ? {'Type'=>'Point','coordinates'=>[station.lng,station.lat]} : nil
       }
     }.to_json
   end
